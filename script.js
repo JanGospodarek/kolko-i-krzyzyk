477442524e8afa771e824f3c7d15c3e89bed87ca
JanGospodarek <jan.gospo@onet.pl>
2022-06-14 12:50:24 +0200

    final

--- script.js
+++ script.js
@@ -6,68 +6,84 @@ class App {
   curSymbol = "x";
   board = new Array(9).fill("");
   winner = undefined;
+  btn = undefined;
   stopGame = false;
+  move = 0;
   constructor() {
     this.btnStart.addEventListener("click", this.init.bind(this));
     this.btnReset.addEventListener("click", this.reset.bind(this));
   }
+
   init() {
+    this.unblur();
     this.container.addEventListener("click", (e) => {
-      if (this.stopGame) return;
-      const btn = e.target.closest(".btnCont");
-      console.log(btn);
-      if (!btn) return;
-      this.writeMove(btn);
-      this.sprawdzenie();
-      this.isWin();
-      this.changeSymbol();
-      console.log(this.buttons, this.board);
+      this.btn = e.target.closest(".btnCont");
+      if (!this.btn || this.stopGame) return;
+      if (this.btn.innerHTML === "") {
+        this.writeMove();
+        this.check();
+        this.isWin();
+        this.isDraw();
+        this.changeSymbol();
+      }
     });
   }
-  sprawdzenie() {
+
+  unblur() {
+    this.container.classList.remove("filter");
+  }
+
+  isDraw() {
+    this.move += 1;
+    if (this.move === 9 && this.stopGame === false && !this.winner) {
+      this.stopGame = true;
+      alert("Remis!");
+    }
+  }
+
+  check() {
     for (let i = 0; i < 3; i++) {
-      //sprawdzenie kolumnm i rzędów
+      //check column and rows
       if (
-        this.znak(
+        this.char(
           this.board[i * 3],
           this.board[i * 3 + 1],
           this.board[i * 3 + 2]
         ) ||
-        this.znak(this.board[i], this.board[i + 3], this.board[i + 6])
+        this.char(this.board[i], this.board[i + 3], this.board[i + 6])
       )
         this.winner = this.curSymbol;
     }
-    //sprawdzenie skosów
+    //check skosów
     if (
-      this.znak(this.board[0], this.board[4], this.board[8]) ||
-      this.znak(this.board[2], this.board[4], this.board[6])
+      this.char(this.board[0], this.board[4], this.board[8]) ||
+      this.char(this.board[2], this.board[4], this.board[6])
     )
       this.winner = this.curSymbol;
   }
-  znak(a, b, c) {
+
+  char(a, b, c) {
     return !!a && a === b && b === c && a === c;
   }
 
   changeSymbol() {
     this.curSymbol === "x" ? (this.curSymbol = "o") : (this.curSymbol = "x");
   }
-  writeMove(btn) {
-    const id = btn.dataset.num;
-    if (btn.innerHTML === "") btn.innerHTML = this.curSymbol;
+
+  writeMove() {
+    const id = this.btn.dataset.num;
+    this.btn.innerHTML = this.curSymbol;
     this.board[id - 1] = this.curSymbol;
   }
+
   isWin() {
     if (this.winner) {
       alert(`Wygrywa ${this.winner}`);
       this.stopGame = true;
     }
   }
+
   reset() {
-    //     this.board.fill("");
-    //     this.buttons.forEach((btn) => {
-    //       btn.innerHTML = " ";
-    //     });
-    //     this.init();
     window.location.reload();
   }
 }
